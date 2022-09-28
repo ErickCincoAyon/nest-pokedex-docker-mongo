e4ef4865dac6b702a1fcb5f8ec0284cdfb506dce
Erick Cinco <erickcincofreelance@gmail.com>
2022-09-28 12:43:56 -0600

    Primer deploy a heroku

--- src/config/joi-schema.validation.ts
+++ src/config/joi-schema.validation.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
     MONGODB: Joi.required(),
-    PORT: Joi.number().default(3001),
+    PORT: Joi.number().default(3000),
     PAGINATION_LIMIT: Joi.number().default(10),
     PAGINATION_OFFSET: Joi.number().default(0),
 })
